8b5470419b8a8bef039cbb772abfcacdb351f3cc
FischkopDev <timo@DESKTOP-GCOF5DG>
2024-06-23 18:42:36 +0200

    Add interaction prototype

---
+++ Prototypes/InteractionPrototype/InteractExample.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractExample : MonoBehaviour, Interactable
+{
+   public void interact()
+    {
+        //execute wathever after ingame interaction
+        Debug.Log("Interaction successfull");
+    }
+}
---
+++ Prototypes/InteractionPrototype/InteractionComponent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Interaction component using Ray tracing to calculate the position of the 
+ * hit object
+ * 
+ * @author
+ *  Timo Skrobanek
+ */
+interface Interactable
+{
+    public void interact();
+}
+
+public class InteractionComponent : MonoBehaviour
+{
+    public Transform interactor; //src transformation
+    public float InteractRange = 5; //Maximum range to interact with object
+
+
+    void Update()
+    {
+        //Check the specific key
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            //send a ray to searched object
+            Ray directedRay = new Ray(interactor.position, interactor.forward);
+
+            //If the obejct is hit, execute the method
+            if (Physics.Raycast(directedRay, out RaycastHit hitInfo, InteractRange))
+            {
+                if (hitInfo.collider.gameObject.TryGetComponent(out Interactable interactObj)) {
+                    interactObj.interact();
+                }
+            }
+        }
+    }
+}
